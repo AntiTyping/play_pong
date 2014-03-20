cbad8baf7e261247e861d94f9571fb6cb60df70b
Andy Pliszka <andy@isp44.com>
2014-03-19 21:40:13 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlayPong::Application.config.secret_key_base = '8273b716e8016bc79a0e5d5c8f80943ccb25e84a4090681d9ee74f941608bb2371965468ef35c4e30f345eef896ccebe934def869d9bc45a600b6fa20cdd3874'
